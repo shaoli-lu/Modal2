e1058bb670c862b0ee90dd37f4e1662cc0da503f
shaoli-lu <shaoli_lu@yahoo.com>
2021-07-19 15:34:12 -0500

    IE testing

--- script.js
+++ script.js
@@ -2,8 +2,19 @@
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 const closeClick = document.getElementById('main')
+const msgBox = document.getElementById('announcement-yellow')
+// const msg = document.getElementsByClassName('msg')
+const isIE = () => {
+    const ua = navigator.userAgent; return ua.indexOf('MSIE') >-1 || ua.indexOf('Trident') > -1;
+}
 
+if (isIE != true) 
+    hideMsg();
+
+
+if (isIE === true)
 openModal();
+
 
 
 closeModalButtons.forEach(button => {
@@ -13,11 +24,13 @@ closeModalButtons.forEach(button => {
     })
 });
 
-closeClick.click(click => {
-    click.addEventListener('click', () => {
+
+$(document).click(function (e) {
+    if ($(e.target).is('#overlay')) {
         closeModal();
-    })
-})
+    }
+
+});
 
 function openModal() {
     modal.classList.add('active')
@@ -29,3 +42,9 @@ function closeModal() {
     modal.classList.remove('active')
     overlay.classList.remove('active')
 };
+
+function hideMsg() {
+    msgBox.style.display = "none";
+    msgBox.innerHTML='';
+    // while (msg.firstChild) msg.removeChild(msg.firstChild);
+}
